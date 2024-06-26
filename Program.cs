bdd6dfd0e635c4a753618a7de341b585b7343bfe
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-06-25 21:38:36 -0300

    first commit
    a basic client register

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CadastroPessoa
+{
+    class Program
+    {
+        static List<Cliente> pessoas = new List<Cliente>();
+        static void Main(string[] args)
+        {
+            bool executando = true;
+            while(executando)
+            {
+                Console.WriteLine("Selecione uma opção:");
+                Console.WriteLine("1 - Adicionar cliente");
+                Console.WriteLine("2 - Visualizar clientes");
+                Console.WriteLine("3 - Editar cliente");
+                Console.WriteLine("4 - Excluir cliente");
+                Console.WriteLine("5 - Sair");
+
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        AdicionarCliente();
+                        break;
+                    case 2:
+                        VisualizarClientes();
+                        break;
+                    case 3:
+                        EditarCliente();
+                        break;
+                    case 4:
+                        ExcluirCliente();
+                        break;
+                    case 5:
+                        executando = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+
+        static void AdicionarCliente()
+        {
+            Console.WriteLine("Digite o nome do cliente: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o email do cliente: ");
+            string email = Console.ReadLine();
+
+            Cliente cliente = new Cliente(nome, email);
+            pessoas.Add(cliente);
+
+            Console.WriteLine("Cliente adicionado com sucesso!");
+        }
+
+        static void VisualizarClientes()
+        {
+            foreach (Cliente cliente in pessoas)
+            {
+                Console.WriteLine($"Nome: {cliente.Nome}");
+                Console.WriteLine($"Email: {cliente.Email}");
+                Console.WriteLine("--------------------------------");
+            }
+        }
+
+        static void EditarCliente()
+        {
+            Console.WriteLine("Digite o nome do cliente que deseja editar: ");
+            string nome = Console.ReadLine();
+
+            Cliente cliente = pessoas.Find(c => c.Nome == nome);
+
+            if(cliente != null)
+            {
+                Console.WriteLine("Digite o novo nome do cliente: ");
+                string novoNome = Console.ReadLine();
+
+                Console.WriteLine("Digite o novo email do cliente: ");
+                string novoEmail = Console.ReadLine();
+
+                cliente.Nome = novoNome;
+                cliente.Email = novoEmail;
+
+                Console.WriteLine("Cliente editado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Cliente não encontrado.");
+            }
+        }
+
+        static void ExcluirCliente()
+        {
+            Console.WriteLine("Digite o nome do cliente que deseja excluir: ");
+            string nome = Console.ReadLine();
+
+            Cliente cliente = pessoas.Find(c => c.Nome == nome);
+
+            if(cliente != null)
+            {
+                pessoas.Remove(cliente);
+                Console.WriteLine("Cliente excluído com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Cliente não encontrado.");
+            }
+        }
+    }
+
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
